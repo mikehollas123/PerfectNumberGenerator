d95b1fe8533cb0706cb79e6ada7ac81f4dc7198c
hollas <hollas@gmail.com>
2020-04-02 09:44:19 -0500

    Add project files.

---
+++ PerfectNumberGenerator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace PerfectNumberGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var gen = new GeneratePerfectNumbers(9);
+            gen.Show();
+            Console.WriteLine(String.Join(", ", gen.Validate()));
+
+             
+
+        }
+    }
+}
